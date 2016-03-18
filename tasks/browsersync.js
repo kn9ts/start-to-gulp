ea4c694873382ba29a0050c28ceba17340a7640d
Eugene Mutai <eugenemutai@gmail.com>
2016-03-18 10:31:13 +0300

    Fixes: houndci issues

--- tasks/browsersync.js
+++ tasks/browsersync.js
@@ -12,9 +12,9 @@ export function browserSync(gulp, plugins, paths) {
     // if no path or the path provided is a directory path
     // spun up a static server
     config.server = {
-      baseDir: paths.serveURL ? paths.serveURL : "./public"
+      baseDir: paths.serveURL ? paths.serveURL : './public'
     }
-  }
+  };
 
   return () => {
     plugins.browserSync.init(config);
