5b3d847c30c76f60c54b9eafd6c03018bedefd29
Eugene Mutai <eugenemutai@gmail.com>
2016-03-18 13:01:47 +0300

    Fixes: renamed and optimised the browserify gulp task

--- tasks/scripts.browserify.js
+++ tasks/scripts.browserify.js
@@ -1,5 +1,7 @@
-export function browserify(gulp, plugins) {
-  const cachebust = new plugins.cachebust();
+export function transformScriptsFromBrowserify(gulp, plugins) {
+  console.log('Enviroment: ' + String(process.env.ENV));
+  const production = (process.env.ENV == 'production');
+
   return () => {
     var b = plugins.browserify({
       entries: './app/scripts/main.js',
@@ -18,17 +20,21 @@ export function browserify(gulp, plugins) {
 
     return b.bundle()
       .pipe(plugins.source('main.js'))
-      .pipe(plugins.buffer())
-      .pipe(cachebust.resources())
-      // .pipe(plugins.uglify())
-      .on('error', plugins.util.log)
       // vinyl-source-stream makes the bundle compatible with gulp
-      .pipe(plugins.rename('main.js'))
+      .pipe(plugins.buffer())
+      .pipe(plugins.rename(production ? 'main.min.js' : 'main.js'))
       .pipe(plugins.sourcemaps.init({
         loadMaps: true
       }))
+      // Add transformation tasks to the pipeline here.
+      .pipe(plugins.if(production, plugins.uglify()))
+      // .pipe(plugins.if(production, plugins.rename('main.min.js')))
+      .on('error', plugins.util.log)
       .pipe(plugins.sourcemaps.write('./maps'))
-      .pipe(gulp.dest('./public/js/'));
+      .pipe(gulp.dest('./public/js/'))
+      .pipe(plugins.browserSync.reload({
+        stream: true
+      }));
   }
 };
 
