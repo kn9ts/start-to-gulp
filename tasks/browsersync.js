d43d760710d84106d9d1f3c8ec059583e813c6fe
Eugene Mutai <eugenemutai@gmail.com>
2016-03-18 09:57:52 +0300

    Modified browsersync task to accormodate both cases where user want's to serve up the public dir statically or nodemon is being used

--- tasks/browsersync.js
+++ tasks/browsersync.js
@@ -1,10 +1,18 @@
-export function browserSync(gulp, plugins) {
+export function browserSync(gulp, plugins, url) {
+  var config = {};
+  // if no path or the path provided is a directory path
+  // spun up a static server
+  if (!url || url.indexOf('./')) {
+    config.server = {
+      baseDir: url ? url : "./public";
+    }
+  } else {
+    // else spun up a proxy server
+    config.proxy = url ? url : 'localhost:3000';
+  }
+
   return () => {
-    plugins.browserSync.init({
-      server: {
-        baseDir: "./public"
-      }
-    });
+    plugins.browserSync.init(config);
   };
 }
 
