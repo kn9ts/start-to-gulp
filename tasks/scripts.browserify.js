260a4ac12ff64f2fdfd39df0a3a4f0d51645fc12
Eugene Mutai <eugenemutai@gmail.com>
2016-03-22 12:16:42 +0300

    Modified browserify; added Babel through to support es6

--- tasks/scripts.browserify.js
+++ tasks/scripts.browserify.js
@@ -1,16 +1,20 @@
-export function transformScriptsFromBrowserify(gulp, plugins, paths) {
+export function transformScriptsUsingBrowserify(gulp, plugins, paths) {
   return () => {
     const production = (process.env.ENV == 'production');
     // console.log('Enviroment: ' + String(process.env.ENV));
 
-    var b = plugins.browserify({
-      entries: './app/scripts/main.js',
-      debug: true,
-      paths: [paths.scripts],
-      transform: [plugins.ngAnnotate]
-    });
-
-    return b.bundle()
+    return plugins.browserify({
+        entries: './app/scripts/main.js',
+        debug: true,
+        paths: [paths.scripts],
+        // transform: [ /* plugins.ngAnnotate, */ plugins.babelify]
+      })
+      // Uncomment if you're using ES6 in the browser (using babel)
+      // babel converts ES6 modules' import/export to CommonJS's require/module.exports
+      // You still need to resolve require/module.exports.
+      // If you want to use ES6/CommonJS modules. Thus the use of babelify
+      .transform(plugins.babelify)
+      .bundle()
       .pipe(plugins.source('main.js'))
       // vinyl-source-stream makes the bundle compatible with gulp
       .pipe(plugins.buffer())
@@ -18,15 +22,16 @@ export function transformScriptsFromBrowserify(gulp, plugins, paths) {
       // in PRODUCTION env, it should be minified but retain the name so that
       // one is not required to change the reference of the file in the script src links
       // .pipe(plugins.rename(production ? 'main.min.js' : 'main.js'))
-      .pipe(plugins.sourcemaps.init({
+      .pipe(plugins.if(production, plugins.sourcemaps.init({
         loadMaps: true
-      }))
+      })))
       // Add transformation tasks to the pipeline here.
       .pipe(plugins.if(production, plugins.uglify()))
       // .pipe(plugins.if(production, plugins.rename('main.min.js')))
       .on('error', plugins.util.log)
-      .pipe(plugins.sourcemaps.write('./maps'))
+      .pipe(plugins.if(production, plugins.sourcemaps.write('./maps')))
       .pipe(gulp.dest(process.env.PROJECT_BUILD_FOLDER + '/js'))
+      .pipe(plugins.browserSync.stream({ match: '**/*.js' }))
       .pipe(plugins.size({ title: 'After:', showFiles: true }));
   }
 };
