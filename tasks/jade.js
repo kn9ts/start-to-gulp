55ee27f39529ed1a52704fe1f385bb131bd38a28
Eugene Mutai <eugenemutai@gmail.com>
2016-03-23 12:21:27 +0300

    Added prettification of the HTML after being compiled from JADE

--- tasks/jade.js
+++ tasks/jade.js
@@ -1,13 +1,10 @@
 export function compileJade(gulp, plugins, paths) {
-  var LOCAL_SETTINGS = {
-    pretty: true
-  };
-
   return () => {
-    gulp.src(paths.jade)
-      .pipe(plugins.jade({
-        locals: LOCAL_SETTINGS
-      }))
-      .pipe(gulp.dest(process.env.PROJECT_BUILD_FOLDER));
+    return gulp.src(paths.jade)
+      .pipe(plugins.jade())
+      // npm i gulp-prettify -D
+      .pipe(plugins.prettify({ indent_size: 4 }))
+      .pipe(gulp.dest(process.env.PROJECT_BUILD_FOLDER))
+      .pipe(plugins.browserSync.stream({ match: '**/*.html'}));
   };
 };
