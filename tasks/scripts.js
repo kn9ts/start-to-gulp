cbe500f06471a7e805060bc9780756e6dac7b5dd
Eugene Mutai <eugenemutai@gmail.com>
2016-03-25 12:24:43 +0300

    Uglify scripts only on production environment

--- tasks/scripts.js
+++ tasks/scripts.js
@@ -1,17 +1,18 @@
 export function transpileScripts(gulp, plugins, paths) {
-  // Note: Since we are not using useref in the scripts build pipeline,
-  // you need to explicitly list your scripts here in the right order
-  // to be correctly concatenated
   return () => {
+    const production = (process.env.ENV == 'production');
+    // console.log('Enviroment: ' + String(process.env.ENV));
+
     gulp.src(paths.scripts)
       .pipe(plugins.size({ title: 'Before:', showFiles: true }))
       .pipe(plugins.sourcemaps.init())
       .pipe(plugins.babel())
       .pipe(plugins.concat('main.js'))
-      .pipe(plugins.uglify({ preserveComments: 'some' }))
+      .pipe(plugins.if(production, plugins.uglify({ preserveComments: 'some' })))
       // Output files
       .pipe(plugins.sourcemaps.write('./maps'))
       .pipe(gulp.dest(process.env.PROJECT_BUILD_FOLDER + '/js'))
+      .pipe(plugins.browserSync.stream({ match: '**/*.js' }))
       .pipe(plugins.size({ title: 'After:', showFiles: true }));
   };
 };
